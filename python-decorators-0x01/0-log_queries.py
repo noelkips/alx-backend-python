4e049c73d7d89ea38e92db1f112e76d0a1993d5d
Noel Kiprono Langat <noellangat28@gmail.com>
2025-05-25 14:17:26 +0300

    Update 0-log_queries.py

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,12 +1,14 @@
 import sqlite3
 import functools
+from datetime import datetime 
 
 #### decorator to log SQL queries
 def log_queries(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         query = kwargs.get('query') if 'query' in kwargs else args[0] if args else 'UNKNOWN QUERY'
-        print(f"[LOG] Executing SQL Query: {query}")
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"[{timestamp}] Executing SQL Query: {query}")
         return func(*args, **kwargs)
     return wrapper
 
@@ -19,5 +21,6 @@ def fetch_all_users(query):
     conn.close()
     return results
 
+#### fetch users while logging the query
 users = fetch_all_users(query="SELECT * FROM users")
 print(users)
