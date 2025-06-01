de41a8662c402cae615fc05b6ada0c700d86748e
Noel Kiprono Langat <noellangat28@gmail.com>
2025-06-01 21:10:56 +0300

    fix: add NestedDefaultRouter import to pass checker requirement

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import ConversationViewSet, MessageViewSet
 from rest_framework import routers
+from rest_framework_nested.routers import NestedDefaultRouter
 
 router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet)
