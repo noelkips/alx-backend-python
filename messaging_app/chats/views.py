bf2703682e8fd91204f16b660b72e5d4f65a30ab
Noel Kiprono Langat <noellangat28@gmail.com>
2025-06-01 20:43:06 +0300

    Add filters and status handling in viewsets for conversation and message APIs

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status, filters
+from rest_framework.response import Response
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 
@@ -7,17 +8,24 @@ class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['participants__username']
 
     def perform_create(self, serializer):
-        # Automatically add the current user as a participant
         conversation = serializer.save()
         conversation.participants.add(self.request.user)
+
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['sender__username', 'conversation__id']
 
     def perform_create(self, serializer):
-        # Automatically set the sender to the current user
         serializer.save(sender=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        return Response(response.data, status=status.HTTP_201_CREATED)
